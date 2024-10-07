054907fcd5205da96526b006668c8644920847d8
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-10-07 20:14:49 +0700

    chunk embedding and sample query

--- rag.py
+++ rag.py
@@ -1,6 +1,8 @@
 import os
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
 
 def main():
     # get data path
@@ -16,12 +18,21 @@ def main():
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     chunks = text_splitter.split_documents(data)
 
-    # Test if the document is split properly by printing the chunk details
-    print(f"Total number of chunks: {len(chunks)}\n")
-    
-    # Print the content of the first 3 chunks
-    for i, chunk in enumerate(chunks[:3]):
-        print(f"Chunk {i + 1}:\n{chunk.page_content}\n{'-'*40}\n")
+    # convert chunks into embeddings and store in vector database
+    vectordb = Chroma.from_documents(
+        documents = chunks,
+        collection_name = "rag-chroma",
+        embedding = OllamaEmbeddings(model='nomic-embed-text', show_progress=True)
+    )
+
+    # Test retrieval with a sample query
+    query = "What is time management?"
+    results = vectordb.similarity_search(query, k=3)
+
+    print("Top 3 most relevant chunks based on the query:")
+    for i, result in enumerate(results):
+        print(f"Result {i+1}:\n{result.page_content}\n")
+
 
 if __name__ == "__main__":
     main()
